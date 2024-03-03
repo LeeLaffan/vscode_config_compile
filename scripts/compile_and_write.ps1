e7897438d944b2a1c75e86b9c57d70d8c067f566
Lee Laffan <leelaffan97@gmail.com>
2024-03-03 23:10:21 +0000

    added more stuff

--- scripts/compile_and_write.ps1
+++
@@ -1,9 +0,0 @@
-$input_dir = "...\vscode_config_compile\input"
-$compiled_path = "...\vscode_config_compile\output\output.json"
-$vscode_path = "C:\Users\...\AppData\Roaming\Code\User\keybindings.json"
-
-$script_path = "...\vscode_config_compile\main.py"
-
-python $script_path -i $input_dir -o $compiled_path
-
-Copy-Item -Path $compiled_path -Destination $vscode_path
---
+++ scripts/write_clean_to_git.ps1
@@ -0,0 +1,7 @@
+$input_path = "L:\Code\Python\vscode_config_compile\"
+$git_path = "L:\Code\Python\vscode_config_compile_git\"
+
+Copy-Item -Path $input_path -Destination $git_path -Recurse -Force
+Write-Output "Copied to $git_path"
+
+
